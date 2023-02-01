33724d16704455c2501ea9624ca5eb07074bcdc5
sehandev <sehandev@gmail.com>
2023-02-01 15:51:41 +0900

    Move duplicated chrome code to main

--- face-crawler.py
+++ face-crawler.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 import typer
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -17,6 +19,7 @@ TMP_PATH = "/data/dataset/CondensedMovies/metadata/casts.csv"
 
 
 def crawl_google_image(
+    driver: webdriver.Chrome,
     query: str,
     limit: int = 10,
     download_dir: str = "/data/imdb-actor-image",
@@ -33,34 +36,22 @@ def crawl_google_image(
         return
 
     large_limit = count["success"] + int(limit * 1.5)
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
-    chrome_options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
-    )
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=chrome_options,
-    )
-
-    driver.get("https://www.google.co.kr/imghp?hl=en")
     elem = driver.find_element(By.NAME, "q")
     elem.send_keys(query)
     elem.send_keys(Keys.RETURN)
 
-    sleep(2)
+    WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "img.rg_i.Q4LuWd"))
+    )
+
+    img_element_list = list(driver.find_elements(By.CSS_SELECTOR, "img.rg_i.Q4LuWd"))
 
     last_height = driver.execute_script("return document.body.scrollHeight")
-
-    img_element_list = list()
     while len(img_element_list) < large_limit:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        sleep(1)
+        sleep(0.1)
 
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
@@ -73,7 +64,9 @@ def crawl_google_image(
 
         img_element_list = list(
             driver.find_elements(By.CSS_SELECTOR, "img.rg_i.Q4LuWd")
-        )[count["success"] :]
+        )
+
+    img_element_list = img_element_list[count["success"] :]
 
     for img in img_element_list:
         if count["success"] >= limit:
@@ -81,16 +74,22 @@ def crawl_google_image(
 
         try:
             img.click()
+            WebDriverWait(driver, 1).until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        '//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/a/img',
+                    )
+                )
+            )
+            image_src = driver.find_element(
+                By.XPATH,
+                '//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/a/img',
+            ).get_attribute("src")
         except:
             count["fail"] += 1
             continue
 
-        sleep(0.5)
-
-        image_src = driver.find_element(
-            By.XPATH,
-            '//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/a/img',
-        ).get_attribute("src")
         ext = image_src.split("?")[0].split(".")[-1].lower()
 
         if ext in ACCEPTABLE_EXT:
@@ -121,8 +120,22 @@ def main(start: int = 0, end: int = 100):
     actor_list = sorted(list(set(cast_list)))
     actor_list = actor_list[start:end]
 
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
+    )
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options,
+    )
+    driver.get("https://www.google.co.kr/imghp?hl=en")
+
     for actor_name in tqdm(actor_list):
         crawl_google_image(
+            driver,
             query=actor_name,
             limit=10,
             download_dir="/data/imdb-actor-image",
